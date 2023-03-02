2eb08a210ea4d276b6d3ab329e4596290c6c675d
Zuhaib Tariq <zuhaib.tariq@arbisoft.com>
2023-03-02 17:06:18 +0500

    Integrated Web

--- test/pageobjects/base/frontDoorFactory.ts
+++ test/pageobjects/base/frontDoorFactory.ts
@@ -2,6 +2,7 @@ import { OS } from "../../utils/enums"
 import { platform } from "../../utils/envConfigs"
 import { AndroidFrontPage } from "../android/front.page"
 import { IOSFrontPage } from "../ios/front.page"
+import { WebFrontPage } from "../web/front.page"
 import { FrontPage } from "./front.page"
 
 export function getFrontDoor(): FrontPage {
@@ -10,5 +11,7 @@ export function getFrontDoor(): FrontPage {
 			return new AndroidFrontPage()
 		case OS.IOS:
 			return new IOSFrontPage()
+		case OS.WEB:
+			return new WebFrontPage()
 	}
 }
